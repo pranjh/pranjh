6ea40c03896ac7c31cdc1032367228b0e83dae7a
pranjal <pranjal@suntekitsolutions.com>
2022-12-10 22:18:00 +0530

    Array

--- src/array.java
+++ src/array.java
@@ -1,14 +1,14 @@
-class main{
+class array{
     public static void main(String[] args){
 
         int age[] = {22, 25, 18, 47, 61  };
 
         System.out.println("Accessing Elements of Array");
-        System.out.println("Pranjal" + age[0]);
-        System.out.println("Priyanka" + age[1]);
-        System.out.println("Prathamesh" + age[2]);
-        System.out.println("Padmavati" + age[3]);
-        System.out.println("Bhaskar" + age[4]);
+        System.out.println("Pranjal = " +  age[0]);
+        System.out.println("Priyanka =" + age[1]);
+        System.out.println("Prathamesh = " + age[2]);
+        System.out.println("Padmavati = " + age[3]);
+        System.out.println("Bhaskar = " + age[4]);
     }
 
 }
